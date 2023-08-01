663ac6cd48dbd3b8efa65c64e327f7812e4d94f1
Awhona <89755024+Awhona@users.noreply.github.com>
2023-08-01 15:32:55 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function flower() {
+    window.location="flower.html"
+}
+
+function chair() {
+    window.location="chair.html"
+}
+
+function tree() {
+    window.location="tree.html"
+}
+
+function skillet() {
+    window.location="skillet.html"
+}
